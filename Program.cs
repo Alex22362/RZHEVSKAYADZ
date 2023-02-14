7f074e4e0d67f9501f2749f7099ccdc092cfd7e3
Alexandra <rzhevskaya.alya00@mail.ru>
2023-02-14 14:31:52 +0300

    третий коммит

--- Program.cs
+++ Program.cs
@@ -16,3 +16,27 @@ else
     Console.WriteLine("второе число" + num_B + "больше чем первое" + num_A );
 } 
 */
+/*
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("input your first number: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input your second number: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input your third number: ");
+int num_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num_1;
+
+if (num_2 > max)
+ {
+    max = num_2;
+ }
+
+if (num_3 > max)
+ {
+    max = num_3;
+ }
+
+  Console.WriteLine( max );
+*/
